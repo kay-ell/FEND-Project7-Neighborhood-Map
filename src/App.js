c687b5bae4aaf5518eb4fee661b45bfd9e9f4d11
Karine <kayell0102@gmail.com>
2019-02-07 08:26:11 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Map from './components/Map';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.initMap = this.initMap.bind(this)
+  }
+  componentDidMount() {
+    const script = document.createElement('script');
+    const API = 'AIzaSyDoLtOi_ugdG-NpBw3oKrGSVL4Xgyj0qy0';
+    script.async = true;
+    script.defer = true;
+    script.src = `https://maps.googleapis.com/maps/api/js?libraries=places,geometry,drawing&key=${API}&v=3&callback=initMap`;
+    document.body.appendChild(script);
+    script.onerror = function(error) {
+      alert('There was an error loading the Google Map: ' + error);
+    };
+    window.initMap = this.initMap;
+  }
+
+  initMap() {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.7831, lng: -73.9712},
+      zoom: 13
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Map
+
+      />
     );
   }
 }
---
+++ src/components/Map.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  
+
+  render() {
+    return (
+      <div id="map" aria-label="Map" role="application" tabIndex="-1" />
+    );
+  }
+}
+
+export default Map;
