629bf97db95579eb907ffcab1b3b36c6814eaec6
Karine <kayell0102@gmail.com>
2019-02-07 20:55:14 -0500

    added info window

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,10 +5,14 @@ class Map extends Component {
     super(props);
     this.initMap = this.initMap.bind(this);
     this.loadMap = this.loadMap.bind(this)
+    this.loadPlaces = this.loadPlaces.bind(this);
+    this.state = {
+      venues: []
+    }
   }
 
   componentDidMount() {
-    this.loadMap();
+    this.loadPlaces();
   }
 
   loadMap() {
@@ -29,9 +33,52 @@ class Map extends Component {
       center: {lat: 40.7831, lng: -73.9712},
       zoom: 13
     })
+
+
+
+    const infowindow = new window.google.maps.InfoWindow();
+
+    // display markers
+    this.state.venues.forEach(venue => {
+      let contentString = `<div id="content"><h3>${venue.name}</h3><p>${venue.location.address}<br>${venue.location.city}, ${venue.location.state}&nbsp;${venue.location.postalCode}<br>${venue.location.country}</p></div>`
+
+      let marker = new window.google.maps.Marker({
+        position: { lat: venue.location.lat, lng: venue.location.lng },
+        map: map,
+        venue: venue,
+        id: venue.id,
+        name: venue.name,
+        animation: window.google.maps.Animation.DROP
+      })
+
+      // listen for marker click
+      marker.addListener('click', function() {
+        infowindow.setContent(contentString);
+        infowindow.open(map, marker);
+      })
+    });
+
+
+
   }
 
-
+  loadPlaces() {
+    let near = 'New York, NY';
+    let query = 'museum';
+    let client_id = 'EHAVVEFJT2BWVFJGCX4FRCQHOU33ZUBU0EH3WDFITZRCPX3L';
+    let client_secret = 'N5IEQ1EP12OI5PR42Y12Z0GSVGJN5BWVDFFA3W4KR1X2TUFS';
+    var apiURL = `https://api.foursquare.com/v2/venues/search?client_id=${client_id}&client_secret=${client_secret}&v=20180323&near=${near}&query=${query}`;
+    return fetch(apiURL)
+    .then(resp => resp.json())
+    .then(myJson => {
+      console.log(myJson);
+      let venues = myJson.response.venues;
+      this.setState({ venues: venues }, this.loadMap());
+    })
+    .catch(error => {
+      console.log('Error! ' + error)
+    });
+  }
 
   render() {
     return (
